a3033d50fd19723e02f38d22baebc43f5872710c
soichisumi <soichi.sumi@gmail.com>
2020-05-10 00:11:18 +0900

    :+1: Update: add request log

--- pkg/health/health.go
+++ pkg/health/health.go
@@ -12,10 +12,12 @@ func NewHealthServer() *HealthServer {return &HealthServer{}}
 type HealthServer struct {}
 
 func (h HealthServer) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+	logger.Info("health check")
 	return &grpc_health_v1.HealthCheckResponse{Status: grpc_health_v1.HealthCheckResponse_SERVING}, nil
 }
 
 func (h HealthServer) Watch(req *grpc_health_v1.HealthCheckRequest, s grpc_health_v1.Health_WatchServer) error {
+	logger.Info("health check")
 	if err := s.Send(&grpc_health_v1.HealthCheckResponse{Status: grpc_health_v1.HealthCheckResponse_SERVING }); err != nil {
 		logger.Error(err.Error(), zap.Error(err))
 		return err
